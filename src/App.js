0a2ad74cd339d1ee7059991a59864d373a98052a
ManoranjanJena24 <mjena@mitaoe.ac.in>
2023-07-15 22:41:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  a = 'Manoranjan';
+
+
+  render() {
+    return (
+      <div>
+        hello my first class basd component -{this.a}
+
+      </div>
+    )
+  }
+}
