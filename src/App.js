14e3d75f91d7b11cb791db7f0b8fe96508a3fa18
ManoranjanJena24 <mjena@mitaoe.ac.in>
2023-07-31 15:10:35 +0530

    refactored news component

--- src/App.js
+++ src/App.js
@@ -15,21 +15,21 @@ import {
 export default class App extends Component {
 
 
-
+  pageSize = 6
   render() {
     return (
       <div>
         <Router>
           <NewsNavBar />
           <Switch>
-            <Route exact path="/"><News Key="general" pageSize={6} country="in" category="general" /></Route>
-            <Route exact path="/business"><News Key="business" pageSize={6} country="in" category="business" /></Route>
-            <Route exact path="/entertainment"><News Key="entertainment" pageSize={6} country="in" category="entertainment" /></Route>
-            <Route exact path="/general"><News Key="general" pageSize={6} country="in" category="general" /></Route>
-            <Route exact path="/health"><News Key="health" pageSize={6} country="in" category="health" /></Route>
-            <Route exact path="/science"><News Key="science" pageSize={6} country="in" category="science" /></Route>
-            <Route exact path="/sports"><News Key="sports" pageSize={6} country="in" category="sports" /></Route>
-            <Route exact path="/technology"><News Key="technology" pageSize={6} country="in" category="technology" /></Route>
+            <Route exact path="/"><News Key="general" pageSize={this.pageSize} country="in" category="general" /></Route>
+            <Route exact path="/business"><News Key="business" pageSize={this.pageSize} country="in" category="business" /></Route>
+            <Route exact path="/entertainment"><News Key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" /></Route>
+            <Route exact path="/general"><News Key="general" pageSize={this.pageSize} country="in" category="general" /></Route>
+            <Route exact path="/health"><News Key="health" pageSize={this.pageSize} country="in" category="health" /></Route>
+            <Route exact path="/science"><News Key="science" pageSize={this.pageSize} country="in" category="science" /></Route>
+            <Route exact path="/sports"><News Key="sports" pageSize={this.pageSize} country="in" category="sports" /></Route>
+            <Route exact path="/technology"><News Key="technology" pageSize={this.pageSize} country="in" category="technology" /></Route>
           </Switch>
         </Router>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -12,7 +12,7 @@ export class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{discription}</p>
-                        <p className="card-text"><small className="text-body-secondary">By {!author ? "unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                        <p className="card-text"><small className="text-danger">By {!author ? "unknown" : author} on {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">ReadMore</a>
                     </div>
                 </div>
